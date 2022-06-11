45efd94ac6e21feab1b33202d6486a8ba995214b
habiburnobel <habiburnobel@gmail.com>
2022-06-11 18:37:19 +0600

    article exists confirmation

--- components/Article/Article.js
+++ components/Article/Article.js
@@ -3,7 +3,9 @@ import React from "react";
 const Article = () => {
   return (
     <div>
-      <h1>hi</h1>
+      <h1 className="text-center my-10 text-3xl font-semibold text-green-500">
+        Article Exists!!!
+      </h1>
     </div>
   );
 };
